7a97d6e115924c646a50e5d8fe3c419b17b606f2
roslava <48380940+roslava@users.noreply.github.com>
2020-04-14 16:56:23 +0300

    Updates

---
+++ precache-manifest.e9a88230a1010131084146847a88d744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039bb28739f28bfcf2aa515241cd74fa",
+    "url": "/newspaper/index.html"
+  },
+  {
+    "revision": "d705a50414087645cb9b",
+    "url": "/newspaper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8f79b9ce428a58511b3",
+    "url": "/newspaper/static/js/2.faf88963.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newspaper/static/js/2.faf88963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d705a50414087645cb9b",
+    "url": "/newspaper/static/js/main.33635bcc.chunk.js"
+  },
+  {
+    "revision": "892eb9e205d19bf8ecc4",
+    "url": "/newspaper/static/js/runtime-main.377a0934.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newspaper/static/media/logo.5d5d9eef.svg"
+  }
+]);
