1956709f493ba99b5f54d977598682ee88702db0
roslava <48380940+roslava@users.noreply.github.com>
2020-04-14 17:03:21 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+
+const articles = [
+  {
+    rubric: `Рубрика`,
+    title: `Заголовок_1`,
+    undertitle: `Подзаголовок_1`,
+    lead: `Текст лида`,
+    text: `Сам текст`,
+    author: `Иванов`,
+    cover: `img`,
+  },
+  {
+    rubric: `Рубрика`,
+    title: `Заголовок_2`,
+    undertitle: `Подзаголовок_2`,
+    lead: `Текст лида`,
+    text: `Сам текст`,
+    author: `Петров`,
+    cover: `img`,
+  }
+];
+
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
+};
+
+const AboutPage = () => {
+  return <h3>About Page</h3>;
+};
+
+const articlesPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <div>{article.rubric}</div>
+          <div>{article.title}</div>
+          <div>{article.lead}</div>
+          <Link to={`/article/${index + 1}`}>Читать полность</Link>
+          <hr/>
+        </div>
+      ))}
     </div>
   );
+};
+
+
+const ArticlePage = ({ match, location }) => {
+  const {
+    params: { articleId }
+  } = match;
+
+  return (
+    <div>
+      <p>
+        <div>{articles[articleId - 1].rubric}</div>
+        <div>{articles[articleId - 1].title}</div>
+        <div>{articles[articleId - 1].lead}</div>
+        <div>{articles[articleId - 1].text}</div>
+        <div>{articles[articleId - 1].author}</div>
+      </p>
+    </div>
+  );
+};
+
+
+
+class App extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Router>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/articles">articles</Link>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/articles" component={articlesPage} />
+          <Route exact path="/article/:articleId" component={ArticlePage} />
+          <Route exact path="/about" component={AboutPage} />
+        </Router>
+        {/*<a href="/articles"> Читать другие матералы номера газеты</a>*/}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
