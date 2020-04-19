6dae18a1e5cf8acafd9ff8b0359fa4a9b00c9a5f
roslava <48380940+roslava@users.noreply.github.com>
2020-04-19 18:00:34 +0300

    Updates

--- precache-manifest.da8f86a59647f32a921f80d98d934036.js
+++ precache-manifest.da8f86a59647f32a921f80d98d934036.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f88e3a65a81741383a1467a619d5972",
+    "revision": "ef6633fb92c549f41552066c9e9ff2b4",
     "url": "/newspaper/index.html"
   },
   {
-    "revision": "acb7b44b6cdf007677e0",
+    "revision": "fa2dfd231d2c03280193",
     "url": "/newspaper/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/newspaper/static/js/2.4421e5a6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "acb7b44b6cdf007677e0",
-    "url": "/newspaper/static/js/main.d6d5b260.chunk.js"
+    "revision": "fa2dfd231d2c03280193",
+    "url": "/newspaper/static/js/main.e0b8586a.chunk.js"
   },
   {
     "revision": "892eb9e205d19bf8ecc4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/newspaper/precache-manifest.741db646c65cdfb125ac1338dbfdc58b.js"
+  "/newspaper/precache-manifest.da8f86a59647f32a921f80d98d934036.js"
 );
 
 self.addEventListener('message', (event) => {
